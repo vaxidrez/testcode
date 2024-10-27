dd0c0b26a61ca7fd0005d6d928e24e3a6814ad19
User <user@hackerrank.workspace>
2024-10-27 11:00:34 +0000

    Add initial repository
    Commit by HackerRank

---
+++ ProductOrderApi/Controllers/OrdersController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductOrderApi.Data.Entities;
+using ProductOrderApi.Data.Models;
+using ProductOrderApi.Services;
+
+namespace ProductOrderApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly OrderService _orderService;
+        public OrdersController(OrderService orderService)
+        {
+            _orderService = orderService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Order>>> GetOrders()
+        {
+            throw new NotImplementedException();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Order>> GetOrder(int id)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPost]
+        public async Task<ActionResult<Order>> AddOrder(CreateOrderModel model)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateOrder(int id, Order order)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrder(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProductOrderApi/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductOrderApi.Data.Entities;
+using ProductOrderApi.Helpers;
+using ProductOrderApi.Services;
+
+namespace ProductOrderApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductService _productService;
+        public ProductsController(ProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPost]
+        public async Task<ActionResult<Product>> AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProduct(int id, Product product)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            throw new NotImplementedException();
+        }       
+    }
+}
